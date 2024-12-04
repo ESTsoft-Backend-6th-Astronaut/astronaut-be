89fdc966b3ffccf4b183fae526d2a418fcc5dc56
cjy <cjiyoon1130@gmail.com>
2024-12-04 09:25:04 +0900

    Feat: PropertySource로 env.properties 추가 및 API 키 위치 변경

--- src/main/java/com/estsoft/astronautbe/AstronautBeApplication.java
+++ src/main/java/com/estsoft/astronautbe/AstronautBeApplication.java
@@ -2,8 +2,10 @@ package com.estsoft.astronautbe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:env.properties")
 public class AstronautBeApplication {
 
 	public static void main(String[] args) {
