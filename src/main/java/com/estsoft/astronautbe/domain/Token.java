c083fa7dbde28fc3492a0074e2b9109fb08d2552
pppppark <juhyoung19990205@gmail.com>
2024-12-04 10:27:03 +0900

    FEAT : User, Token Entity 추가

---
+++ src/main/java/com/estsoft/astronautbe/domain/Token.java
@@ -0,0 +1,33 @@
+package com.estsoft.astronautbe.domain;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "token")
+@Getter
+@NoArgsConstructor
+public class Token {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "token_id", nullable = false)
+    private Integer tokenId;
+
+    @Column(name = "users_id", nullable = false)
+    private String quoteContent;
+
+    @Column(name = "refresh_token", columnDefinition = "TEXT", nullable = false)
+    private String refreshToken;
+
+    @Column(name = "expires_at", columnDefinition = "TIMESTAMP", nullable = false)
+    private Timestamp expiresAt;
+
+    @CreatedDate
+    @Column(name = "created_at", columnDefinition = "TIMESTAMP", nullable = false)
+    private String createdAt;
+}
