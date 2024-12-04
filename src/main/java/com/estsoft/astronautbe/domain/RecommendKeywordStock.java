15374932a9cbe05a07a9655b673689099250e716
cjyjiyoon <cjiyoon1130@gmail.com>
2024-12-04 10:38:51 +0900

    FEAT: table명 추가

--- src/main/java/com/estsoft/astronautbe/domain/RecommendKeywordStock.java
+++ src/main/java/com/estsoft/astronautbe/domain/RecommendKeywordStock.java
@@ -9,6 +9,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,6 +20,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "recommend_keyword_stock")
 public class RecommendKeywordStock {
 
 	@Id
--- src/main/java/com/estsoft/astronautbe/domain/SearchVolume.java
+++ src/main/java/com/estsoft/astronautbe/domain/SearchVolume.java
@@ -11,6 +11,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,6 +22,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "search_volume")
 public class SearchVolume {
 
 	@Id
