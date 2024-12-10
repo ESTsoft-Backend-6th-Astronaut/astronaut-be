d93b75097d6c712bd5f34ccdf4e5f215e41590cc
cjy <95923414+cjyjiyoon@users.noreply.github.com>
2024-12-10 14:13:04 +0900

    FIX: 파일 버전 오류 해결

--- src/main/java/com/estsoft/astronautbe/domain/KeywordNews.java
+++ src/main/java/com/estsoft/astronautbe/domain/KeywordNews.java
@@ -9,12 +9,16 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "keyword_news")
@@ -27,12 +31,17 @@ public class KeywordNews {
 	@Column(name = "news_id", nullable = false)
 	private Long newsId;
 
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "keyword_id", referencedColumnName = "keyword_id", nullable = false)
+	private Keyword keyword;
+
 	@Column(name = "title", columnDefinition = "VARCHAR(255)", nullable = false)
 	private String title;
 
 	@Column(name = "newspaper", columnDefinition = "VARCHAR(255)", nullable = false)
 	private String newspaper;
 
+	@Setter
 	@Column(name = "original_link", columnDefinition = "VARCHAR(255)", nullable = false)
 	private String originalLink;
 
@@ -47,12 +56,27 @@ public class KeywordNews {
 
 	@Column(name = "created_at", columnDefinition = "TIMESTAMP", nullable = false)
 	@CreatedDate
-	private Timestamp created_at;
+	private Timestamp createdAt;
 
 	@Column(name = "updated_at", columnDefinition = "TIMESTAMP", nullable = false)
 	@UpdateTimestamp
-	private Timestamp updated_at;
+	private Timestamp updatedAt;
+
+	public KeywordNews(Keyword keyword, String title, String newspaper, String originalLink, Timestamp pubDate,
+		Boolean emotion,
+		String summary) {
+		this.keyword = keyword;
+		this.title = title;
+		this.newspaper = newspaper != null ? newspaper : "";
+		this.originalLink = originalLink;
+		this.pubDate = pubDate;
+		this.emotion = emotion != null ? emotion : false;
+		this.summary = summary != null ? summary : "";
+	}
+
+	public void updateAIInfo(String newspaper, Boolean emotion, String summary) {
+		this.newspaper = newspaper;
+		this.emotion = emotion;
+		this.summary = summary;
+	}
 }
-
-
-
