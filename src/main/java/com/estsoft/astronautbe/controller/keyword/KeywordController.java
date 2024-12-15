1ef72eb1106f0667172589521bad4ee722572353
cjyjiyoon <cjiyoon1130@gmail.com>
2024-12-15 23:33:33 +0900

    FEAT: React에 연결할 수 있도록 추천 키워드 종목/종목 검색량 서비스/컨트롤러 전체 흐름 및 코드 수정

--- src/main/java/com/estsoft/astronautbe/controller/keyword/KeywordController.java
+++ src/main/java/com/estsoft/astronautbe/controller/keyword/KeywordController.java
@@ -1,20 +1,23 @@
 package com.estsoft.astronautbe.controller.keyword;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.estsoft.astronautbe.domain.RecommendKeywordStock;
-import com.estsoft.astronautbe.domain.SearchVolume;
+
+import com.estsoft.astronautbe.domain.dto.RecommendKeywordStockDTO;
+import com.estsoft.astronautbe.domain.dto.SearchVolumeWithStockDTO;
 import com.estsoft.astronautbe.repository.RecommendKeywordStockRepository;
 import com.estsoft.astronautbe.service.KeywordService;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:3000", "http://127.0.0.1:3000"})
 public class KeywordController {
 
 	private final KeywordService keywordService;
@@ -26,37 +29,33 @@ public class KeywordController {
 		this.recommendKeywordStockRepository = recommendKeywordStockRepository;
 	}
 
-	// GET -  키워드를 받아 Alan으로 추천 종목 5개 추출 API
+	// GET -  키워드를 받아 DB의 추천 종목 5개 추출 API
 	@GetMapping("/api/keywords/{keyword_id}/recommend")
-	public ResponseEntity<List<RecommendKeywordStock>> getRecommendKeywordStock(
+	public List<RecommendKeywordStockDTO> getRecommendKeywordStock(
 			@PathVariable("keyword_id") Long keywordId) {
-
-		String keyword = keywordService.getKeywordNameById(keywordId);
-
-		List<RecommendKeywordStock> recommendKeywordStocks = keywordService.getRecommendKeywordStock(keyword, keywordId);
-
-		return ResponseEntity.ok(recommendKeywordStocks);
+		return keywordService.getRecommendStocks(keywordId);
 	}
 
-	// POST - 종목 리스트를 받아 네이버 검색량 조회 API
-	@PostMapping("/api/keywords/{keyword_id}")
-	public ResponseEntity<List<SearchVolume>> getTrends(@PathVariable("keyword_id") Long keywordId,
-			@RequestBody List<String> stockNames) {
+	// POST - 종목 리스트를 받아 DB에 저장한 네이버 검색량 조회 API
+	@GetMapping("/api/keywords/{keyword_id}")
+	public List<SearchVolumeWithStockDTO> getTrends(@PathVariable("keyword_id") Long keywordId) {
 
-		// 추천 종목 정보
-		List<RecommendKeywordStock> recommendKeywordStocks = recommendKeywordStockRepository.findByKeyword_KeywordId(
-				keywordId);
-
-		List<SearchVolume> searchVolumes = keywordService.getSearchAmount(stockNames, keywordId, recommendKeywordStocks);
-
-		return ResponseEntity.ok(searchVolumes);
+		return keywordService.getRecommendStockWithSearchVolumes(keywordId);
 	}
 
-	// 전체 프로세스 실행
+	// 전체 프로세스 실행 (1일 1회 사용하도록 스케줄러로 설정 필요)
 	@PostMapping("/api/keywords/allProcess")
 	public ResponseEntity<String> processRecommendStockAndSearch() {
 		keywordService.processKeywordRecommendation();
 		return ResponseEntity.ok("전체 프로세스 실행 완료");
+	}
+
+	// 오늘의 키워드 ID 반환(리액트 연결용)
+	@GetMapping("/api/keywords/today")
+	public ResponseEntity<List<Long>> getTodayKeywordsIds() {
+		LocalDateTime today = LocalDateTime.now();
+		List<Long> todayKeywordIds = keywordService.getTodayKeywordIds(today);
+		return ResponseEntity.ok(todayKeywordIds);
 	}
 
 }
