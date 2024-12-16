062c0bde3064fed9a535a5becb88bb3431668aa8
cjy <95923414+cjyjiyoon@users.noreply.github.com>
2024-12-16 14:51:36 +0900

    FEAT: 생성자 추가, @EntityListeners 추가

--- src/main/java/com/estsoft/astronautbe/domain/RecommendKeywordStock.java
+++ src/main/java/com/estsoft/astronautbe/domain/RecommendKeywordStock.java
@@ -3,9 +3,11 @@ package com.estsoft.astronautbe.domain;
 import java.time.LocalDateTime;
 
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -24,6 +26,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "recommend_keyword_stock")
+@EntityListeners(AuditingEntityListener.class)
 public class RecommendKeywordStock {
 
 	@Id
@@ -62,5 +65,12 @@ public class RecommendKeywordStock {
 
 	public String getStockCode() {
 		return this.stock != null ? this.stock.getStockCode() : null;
+	}
+
+	public RecommendKeywordStock(Long recommendStockId, Keyword keyword, Stock stock, String reason) {
+		this.recommendStockId = recommendStockId;
+		this.keyword = keyword;
+		this.stock = stock;
+		this.reason = reason;
 	}
 }
