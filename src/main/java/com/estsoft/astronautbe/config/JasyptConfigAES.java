6197c7aaad5ed45da3c2df2247fc136df22eee34
tmfrl99 <99esg@naver.com>
2024-12-19 10:13:29 +0900

    Fix: 환경변수 설정

--- src/main/java/com/estsoft/astronautbe/config/JasyptConfigAES.java
+++ src/main/java/com/estsoft/astronautbe/config/JasyptConfigAES.java
@@ -3,6 +3,7 @@ package com.estsoft.astronautbe.config;
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,11 +12,14 @@ import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties
 @Configuration
 @EnableEncryptableProperties
 public class JasyptConfigAES {
+	@Value("${jasypt.encryptor.key}")
+	private String passwordKey;
+
 	@Bean("jasyptEncryptorAES")
 	public StringEncryptor stringEncryptor() {
 		PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
 		SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-		config.setPassword(System.getenv("JASYPTKEY"));     // 지정한 암호화 키
+		config.setPassword(passwordKey);     // 지정한 암호화 키
 		config.setAlgorithm("PBEWITHHMACSHA512ANDAES_256");    // 알고리즘
 		config.setKeyObtentionIterations("1000");    // 반복할 해싱 회수
 		config.setPoolSize("1");    // 인스턴스 pool
